d243c7b075af7b4fd68f28ae890ee3bce7454165
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2024-09-05 11:24:30 +0530

    status part done

--- src/components/partials/StatusShow.jsx
+++ src/components/partials/StatusShow.jsx
@@ -1,14 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const StatusShow = () => {
+  const navigate = useNavigate();
   const { userId } = useParams();
 
   const { allStatus } = useSelector((state) => state.statusReducer);
   const statusUser = allStatus && allStatus.find((s) => s.user?._id === userId);
 
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    let interval;
+    if (statusUser) {
+      interval = setInterval(() => {
+        setProgress((prev) => prev + 1);
+      }, 30);
+
+      if (progress >= 100) {
+        setProgress(0);
+
+        if (currentIndex < statusUser.user.status.length - 1) {
+          setCurrentIndex((prevIndex) => prevIndex + 1);
+        } else {
+          navigate("/");
+        }
+      }
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [currentIndex, progress, statusUser]);
+
+  const nextStatusHandler = () => {
+    setProgress(0);
+    if (currentIndex < statusUser.user.status.length - 1) {
+      setCurrentIndex((prev) => prev + 1);
+    } else {
+      navigate("/");
+    }
+  };
+
+  const previousStatusHandler = () => {
+    setProgress(0);
+    if (currentIndex > 0) {
+      setCurrentIndex((prev) => prev - 1);
+    } else {
+      navigate("/");
+    }
+  };
 
   return (
     statusUser && (
@@ -31,14 +74,28 @@ const StatusShow = () => {
               {statusUser.user?.fullName}
             </h1>
           </div>
+          <div className="w-full h-[3px] bg-zinc-600 absolute bottom-0 left-0">
+            <div
+              style={{ width: `${progress}%`, transition: "width 0.1s linear" }}
+              className="h-full rounded-lg bg-white"
+            ></div>
+          </div>
         </div>
-        <div className="w-full h-full bg-red-100 overflow-hidden">
+        <div className="w-full h-full overflow-hidden">
           <img
             className="w-full h-full object-cover"
             src={statusUser.user.status[currentIndex].image.url}
             alt=""
           />
         </div>
+        <div
+          onClick={previousStatusHandler}
+          className="w-1/2 absolute top-0 left-0 h-full"
+        ></div>
+        <div
+          onClick={nextStatusHandler}
+          className="w-1/2 absolute top-0 right-0 h-full"
+        ></div>
         <div className="w-full py-2 px-2 absolute bottom-0 left-0">
           <form className="border-2 border-zinc-100 w-full rounded-full px-2 py-2 flex items-center gap-1">
             <input
