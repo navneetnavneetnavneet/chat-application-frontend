129f1a74be3c04c22f7bce4c973b954905bbbcab
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2024-09-12 17:33:14 +0530

    done

--- src/components/partials/TopNav.jsx
+++ src/components/partials/TopNav.jsx
@@ -9,30 +9,36 @@ const TopNav = () => {
   );
 
   return (
-    <div className="w-full py-2 px-2 border-b border-zinc-400 flex items-center gap-2">
-      <i onClick={() => navigate("/")} className="ri-arrow-left-line"></i>
-      <div className="w-[12vw] h-[12vw] md:w-[4vw] md:h-[4vw] rounded-full overflow-hidden">
-        <img
-          className="w-full h-full object-cover"
-          src={selectedUser && selectedUser.profileImage.url}
-          alt=""
-        />
+    <div className="w-full py-2 px-2 border-b border-zinc-400 flex items-center justify-between gap-2">
+      <div className="flex items-center gap-2">
+        <i onClick={() => navigate("/")} className="ri-arrow-left-line cursor-pointer"></i>
+        <div className="w-[12vw] h-[12vw] md:w-[4vw] md:h-[4vw] rounded-full overflow-hidden">
+          <img
+            className="w-full h-full object-cover"
+            src={selectedUser && selectedUser.profileImage.url}
+            alt=""
+          />
+        </div>
+        <div className="">
+          <h1 className="text-xl font-semibold leading-5">
+            {selectedUser && selectedUser.fullName}
+          </h1>
+          <p
+            className={`${
+              onlineUsers && onlineUsers.includes(selectedUser?._id)
+                ? "text-green-500"
+                : "text-black"
+            } text-sm md:text-xs font-semibold leading-5`}
+          >
+            {onlineUsers && onlineUsers.includes(selectedUser?._id)
+              ? "online"
+              : "offline"}
+          </p>
+        </div>
       </div>
-      <div className="">
-        <h1 className="text-xl font-semibold leading-5">
-          {selectedUser && selectedUser.fullName}
-        </h1>
-        <p
-          className={`${
-            onlineUsers && onlineUsers.includes(selectedUser?._id)
-              ? "text-green-500"
-              : "text-black"
-          } text-sm md:text-xs font-semibold leading-5`}
-        >
-          {onlineUsers && onlineUsers.includes(selectedUser?._id)
-            ? "online"
-            : "offline"}
-        </p>
+      <div className="flex items-center gap-5 text-xl cursor-pointer">
+      <i className="ri-phone-line"></i>
+      <i className="ri-video-add-line"></i>
       </div>
     </div>
   );
